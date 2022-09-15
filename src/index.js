58548ec772e114bb645b19dc48623623e6c26d38
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-15 21:50:12 +0900

    api gg

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
-import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ src/modules/sample.js
@@ -0,0 +1,28 @@
+import { handleActions } from "redux-actions";
+import * as api from "../lib/api";
+
+const GET_POST = "sample/GET_POST";
+const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
+
+const GET_USERS = "sample/GET_USERS";
+const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
+
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST });
+  try {
+    const response = await api.getPost(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+    throw e;
+  }
+};
